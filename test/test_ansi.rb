f44ca08a4483dbb6adb48053b61883d23ece045d
Thomas Aylott <oblivious+git@subtlegradient.com>
2008-07-29 12:52:39 -0400

    Mythical API example

--- test/test_ansi.rb
+++ test/test_ansi.rb
@@ -1,10 +1,14 @@
 require "test/unit"
-
 require "ansi"
+require "ansi_to_png"
 
 class TestAnsi < Test::Unit::TestCase
   def test_basic
-    a = Ansi.new(File.dirname(__FILE__) + '/sph_love.ans')
-    assert a
+    filename = File.dirname(__FILE__) + '/sph_love.ans'
+    
+    a = ANSI.new(filename)
+    png = ANSI_to_PNG::parse(a) # => should return a PNG object
+    
+    png = ANSI_to_PNG::parse(filename) # => should return a PNG object
   end
 end
